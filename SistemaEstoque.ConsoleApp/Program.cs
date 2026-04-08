077fe0d26921152896dc6598b75595a3c49a9ee7
Victor <victor_hugo_moreira@hotmail.com>
2026-04-08 20:35:02 -0300

    first commit

---
+++ SistemaEstoque.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿bool prosseguir = true;
+
+while (prosseguir)
+{
+    Console.WriteLine("---------------------");
+    Console.WriteLine("Controle De Estoque");
+    Console.WriteLine("---------------------");
+}
